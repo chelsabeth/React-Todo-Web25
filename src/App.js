a5d0954984e75147cfd6539bd78be815bb7cc5e5
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-02 16:20:56 -0500

    added notes to components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const List = [
+  {
+
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
